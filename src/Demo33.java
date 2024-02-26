c1ed1cbbd3c01ad3bff4dd388224a117e36be7a4
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-02-26 20:39:16 +0530

    Update README.md file

--- src/Demo33.java
+++ src/Demo33.java
@@ -1,23 +1,53 @@
 void main(){
-    myMethod(5);
+    myMethod(20,30);
 }
 
-//void myMethod(int... x){
-//    System.out.println("A.  myMethod(int...)");
+void myMethod(long... x){
+    System.out.println("A");
+}
+
+void myMethod(Integer... x){
+    System.out.println("B");
+}
+
+//void myMethod(Number... x){
+//    System.out.println("C");
 //}
+
+//void myMethod(Number... x){     // Object > Object[] > Number[] > Integer[]
+//    System.out.println("C.  Number(Integer...)");
+//}
+
+//void myMethod(short x, int...y){
+//    System.out.println("A.");
+//}
+//
+//void myMethod(int x, int y, int...k){
+//    System.out.println("B. ");
+//}
+
+//void myMethod(Integer... x){    // Object > Object[] > Number[] > Integer[]
+//    System.out.println("D. ");
+//}
+//
+//void myMethod(long... x){       // Object > long[]
+//    System.out.println("E. ");
+//}
+
+
 
 //void myMethod(Integer... x){
 //    System.out.println("B.  myMethod(Integer...)");
 //}
 
-//void myMethod(Number... x){
+
+
+//void myMethod(Object... x){
 //    System.out.println("C.  Number(Integer...)");
 //}
 
-void myMethod(Object... x){
-    System.out.println("C.  Number(Integer...)");
-}
 
-void myMethod(Object x){
-    System.out.println("C.  Number(Integer...)");
-}
+
+//void myMethod(Object x){
+//    System.out.println("C.  Number(Integer...)");
+//}
